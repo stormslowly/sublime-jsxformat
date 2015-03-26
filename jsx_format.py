b710c66da1997691b271974caff498a03580242d
stormslowly <stormslowly@gmail.com>
2015-03-26 15:13:21 +0800

    cmd.js works

--- jsx_format.py
+++ jsx_format.py
@@ -4,7 +4,6 @@ import sublime, sublime_plugin
 # debug with view.run_command('jsx_format')
 class JsxFormatCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		# self.view.insert(edit, 0, "Hello, World!")
 		region = sublime.Region(0, self.view.size())
 		code = self.view.substr(region)
 		print(code)
