b69e2cc74c1ee5195bab486d8726c90cdf94d170
stormslowly <stormslowly@gmail.com>
2015-03-27 09:31:03 +0800

    basic working version

--- cmd.js
+++ cmd.js
@@ -1,4 +1,6 @@
 'use strict';
+var util = require('util');
+var stream = require('stream');
 
 var esformatter = require('esformatter');
 esformatter.register(require('esformatter-jsx'));
@@ -6,20 +8,50 @@ esformatter.register(require('esformatter-jsx'));
 var stdinStr = '';
 var buffers = [];
 
+function BufferStream(source) {
+  if (!Buffer.isBuffer(source)) {
+    throw (new Error('Source must be a buffer.'));
+  }
+  stream.Readable.call(this);
+  this._source = source;
+  this._offset = 0;
+  this._length = source.length;
+  this.on('end', this._destroy);
+}
+util.inherits(BufferStream, stream.Readable);
+BufferStream.prototype._destroy = function() {
+  this._source = null;
+  this._offset = null;
+  this._length = null;
+};
+BufferStream.prototype._read = function(size) {
+  if (this._offset < this._length) {
+    this.push(
+      this._source.slice(this._offset, this._offset + size)
+    );
+    this._offset += size;
+  }
+  if (this._offset >= this._length) {
+    this.push(null);
+  }
+};
+
 
 process.stdin.on('data', function(trunk) {
   buffers.push(trunk);
 });
 
-
-
-process.stdin.on('end', function() {
+var endhandle = function() {
   stdinStr = Buffer.concat(buffers).toString();
   var output = esformatter.format(stdinStr);
-  process.stdout.write(output);
-  process.exit(0);
-
-});
+  // console.log(output);
+  new BufferStream(new Buffer(output))
+    .pipe(process.stdout)
+    .on('end', function() {
+      process.exit(0);
+    });
+};
+process.stdin.on('end', endhandle);
 
 process.stdin.on('error', function(err) {
   console.error('ERROR:', err.message);
--- jsx_format.py
+++ jsx_format.py
@@ -1,9 +1,40 @@
 import sublime, sublime_plugin
+import subprocess,os,sys
 
+directory = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(directory)
+
+import merge_util
+
+
+PLUGIN_FOLDER = os.path.dirname(os.path.realpath(__file__))
 
 # debug with view.run_command('jsx_format')
 class JsxFormatCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+
+		formatScript = os.path.join(PLUGIN_FOLDER, 'cmd.js')
+
 		region = sublime.Region(0, self.view.size())
 		code = self.view.substr(region)
-		print(code)
+
+		try:
+			p = subprocess.Popen(['node',formatScript],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE ,shell=True)
+			formatedCode,err = p.communicate(input=code.encode('utf-8'))
+
+			print(formatedCode.decode('utf-8'),err)
+			if err:
+				sublime.status_message('esformat filed');
+				return
+			_, err = merge_util.merge_code(self.view, edit, code, formatedCode.decode('utf-8'))
+			if err:
+				sublime.error_message("JsFormat: Merge failure: '%s'" % err)
+		except Exception as e :
+			print('JSX Format Error:',e)
+			sublime.error_message("JsFormat: Merge failure" )
+			pass
+		else:
+			pass
+		finally:
+			pass
+
